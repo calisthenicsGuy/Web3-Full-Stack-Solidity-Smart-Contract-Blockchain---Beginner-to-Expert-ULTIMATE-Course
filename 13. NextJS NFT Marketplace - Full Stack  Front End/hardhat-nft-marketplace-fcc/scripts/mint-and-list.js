1411d98e2affa2c9576a37884c3cd6b29b42aa72
Radoslav Radev <88380154+calisthenicsGuy@users.noreply.github.com>
2022-10-21 11:43:28 +0300

    next js set up

---
+++ 13. NextJS NFT Marketplace - Full Stack  Front End/hardhat-nft-marketplace-fcc/scripts/mint-and-list.js
@@ -0,0 +1,42 @@
+const { network, ethers, deployments, getNamedAccounts } = require("hardhat");
+
+async function mintAndList() {
+  const { deployer } = await getNamedAccounts();
+  const BASIC_NFT_TOKEN_ID = 0;
+  const BASIC_NFT_TWO_TOKEN_ID = 0;
+  const PRICE = await ethers.utils.parseEther("0.1");
+
+  const nftMarketplace = await ethers.getContract("NftMarketplace", deployer);
+  const basicNft = await ethers.getContract("BasicNft", deployer);
+  const basicNftTwo = await ethers.getContract("BasicNftTwo", deployer);
+
+  console.log("Mint first NFT...");
+  const mintBasicNftTxResponse = await basicNft.mintNft();
+  await mintBasicNftTxResponse.wait(1);
+  console.log("Approve first NFT...");
+  await basicNft.approve(nftMarketplace.address, BASIC_NFT_TOKEN_ID);
+
+  console.log("Mint second NFT...");
+  const mintBasicNftTwoTxResponse = await basicNftTwo.mintNft();
+  await mintBasicNftTwoTxResponse.wait(1);
+  console.log("Approve second NFT...");
+  await basicNftTwo.approve(nftMarketplace.address, BASIC_NFT_TWO_TOKEN_ID);
+
+  console.log("Listing first NFT");
+  await nftMarketplace.listItem(basicNft.address, BASIC_NFT_TOKEN_ID, PRICE);
+  console.log("Listing second NFT");
+  await nftMarketplace.listItem(
+    basicNftTwo.address,
+    BASIC_NFT_TWO_TOKEN_ID,
+    PRICE
+  );
+
+  console.log("--------------------------------------------------");
+}
+
+mintAndList()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
